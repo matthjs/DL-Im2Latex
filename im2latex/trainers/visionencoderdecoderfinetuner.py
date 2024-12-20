6ae5dc2f3e8c48faffabea56a4f5c158f61776ad
Matthijs <matthijs.vanderlende@gmail.com>
2024-12-20 12:56:52 +0100

    changed finetuner to inherit from trainer class and added what is new

--- im2latex/trainers/visionencoderdecoderfinetuner.py
+++ im2latex/trainers/visionencoderdecoderfinetuner.py
@@ -1,17 +1,74 @@
-from im2latex.trainers.abstracttrainer import AbstractTrainer
+from loguru import logger
+from peft import LoraConfig, IA3Config, get_peft_model
+import warnings
+from im2latex.conf.config_classes import Config
+from im2latex.trainers import VisionEncoderDecoderTrainer
+
+warnings.filterwarnings("ignore")
 
 
-class VisionEncoderDecoderFinetuner(AbstractTrainer):
-    # TODO
-    def train(self) -> None:
-        pass
+class VisionEncoderDecoderFinetuner(VisionEncoderDecoderTrainer):
+    """
+    Decoder class for VisionEncoderDecoderTrainer. Change the behavior to perform
+    finetuning instead of full all parameter training.
+    """
+    def __init__(self, cfg: Config):
+        super().__init__(cfg)
+        self.lora_config = None
+        self.ia3_config = None
 
-    def evaluate(self) -> 'AbstractTrainer':
-        pass
+    def setup_finetuning(self) -> None:
+        """
+        Sets up finetuning configurations for LoRA and IA³ adapters.
+        """
+        # setting up the adapter
+        self.lora_config = LoraConfig(
+            r=16,
+            lora_alpha=16,
+            target_modules=[
+                # Encoder (Swin Transformer) modules
+                "attn.qkv",
+                "attn.proj",
+                "mlp.fc1",
+                "mlp.fc2",
+                # Decoder (GPT-2) modules
+                "c_attn",
+                "c_proj",
+                "c_fc",
+                "attn.c_proj",
+            ],
+            lora_dropout=0.1,
+            bias="none"
+            # task_type="VL"  # Vision-Language task
+        )
 
-    def save_model(self, checkpoint_path: str) -> 'AbstractTrainer':
-        pass
+        # setting up the IA³ adapter
+        # I do not recall this being used anywhere but have included it for now.
+        self.ia3_config = IA3Config(
+            target_modules=[
+                # Encoder (Swin Transformer) modules
+                "attn.qkv",
+                "attn.proj",
+                "mlp.fc1",
+                "mlp.fc2",
+                # Decoder (GPT-2) modules
+                "c_attn",
+                "c_proj",
+                "c_fc",
+                "attn.c_proj",
+            ],
+            feedforward_modules=[
+                "mlp.fc1",
+                "mlp.fc2",
+                "c_fc",
+            ],
+            init_ia3_weights=True,
+            # task_type="VL"  # Vision-Language task
+        )
 
-    def load_model(self, checkpoint_path) -> 'AbstractTrainer':
-        pass
+        self.model = get_peft_model(self.model, self.lora_config)
+
+        if self.cfg.log_level >= 1:
+            logger.debug("Finetuning")
+            self.model.print_trainable_parameters()
 
--- im2latex/trainers/visionencoderdecodertrainer.py
+++ im2latex/trainers/visionencoderdecodertrainer.py
@@ -11,13 +11,6 @@ from transformers import (
     SwinConfig,
     GPT2Config,
     AutoTokenizer,
-    AutoFeatureExtractor
-)
-from transformers import (
-    VisionEncoderDecoderModel,
-    SwinConfig,
-    GPT2Config,
-    AutoTokenizer,
     AutoFeatureExtractor,
     get_linear_schedule_with_warmup
 )
@@ -35,121 +28,33 @@ from transformers import logging
 # Stop intialization of weights warning
 logging.set_verbosity_error()
 
+
 class VisionEncoderDecoderTrainer(AbstractTrainer):
     """
     A trainer class for training and evaluating a VisionEncoderDecoderModel.
     """
-    
-    def setup_model(self):
-        """
-        Set up the VisionEncoderDecoderModel and tokenizer
-        """
-        
-        self.tokenizer = AutoTokenizer.from_pretrained(self.cfg.model.tokenizer_name)
-        self.tokenizer.pad_token = self.tokenizer.eos_token
-
-        self.feature_extractor = AutoFeatureExtractor.from_pretrained(self.cfg.model.feature_extractor)
-        
-        # Set up te vision encoder decoder model.
-        self.model = VisionEncoderDecoderModel.from_encoder_decoder_pretrained(
-            self.cfg.model.encoder_name,
-            self.cfg.model.decoder_name
-        )
-        
-        self.model.config.decoder_start_token_id = self.tokenizer.bos_token_id
-        self.model.config.pad_token_id = self.tokenizer.pad_token_id
-        self.model.config.eos_token_id = self.tokenizer.eos_token_id
-
-        self.model.config.no_repeat_ngram_size = self.cfg.decoding.no_repeat_ngram_size
-        self.model.config.length_penalty = self.cfg.decoding.length_penalty
-        
-        self.model.config.max_length = self.cfg.decoding.max_length
-        self.model.config.early_stopping = self.cfg.decoding.early_stopping
-        self.model.config.num_beams = self.cfg.decoding.num_beams
-        
-        self.model.decoder.resize_token_embeddings(len(self.tokenizer))
-
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        logger.info(f"Using device: {self.device}")
-
-        self.model.to(self.device)
-        torch.compile(self.model)
-        
-    def setup_dataset(self):
-        
-        dataset = load_dataset(self.cfg.dataset.train_dataset_path, self.cfg.dataset.split_dataset_name)
-        train_val_split = dataset["train"].train_test_split(test_size=self.cfg.dataset.val_test_size, seed=self.cfg.torch.seed)
-        train_ds = train_val_split["train"]
-        val_test_split = train_val_split["test"].train_test_split(test_size=0.5, seed=self.cfg.torch.seed)
-        val_ds = val_test_split["train"]
-        test_ds = val_test_split["test"]
-        
-        image_size = self.cfg.image.image_size
-
-        self.train_dataset = LatexDataset(train_ds, 
-                                          self.tokenizer,
-                                          self.feature_extractor,
-                                          phase='train',
-                                          image_size=image_size)
-        
-        self.val_dataset = LatexDataset(val_ds, self.tokenizer,
-                                        self.feature_extractor,
-                                        phase='val',
-                                        image_size=image_size)
-        
-        self.test_dataset = LatexDataset(test_ds,
-                                         self.tokenizer, 
-                                         self.feature_extractor, 
-                                         phase='test',
-                                         image_size=image_size)
-
-        train_sampler = RandomSampler(self.train_dataset)
-        val_sampler = SequentialSampler(self.val_dataset)
-        test_sampler = SequentialSampler(self.test_dataset)
-
-        DataCollator.padding_value = self.tokenizer.pad_token_id
-        
-        self.train_dataloader = DataLoader(self.train_dataset, 
-                                           batch_size=self.cfg.training.batch_size_train,
-                                           sampler=train_sampler,
-                                           collate_fn=DataCollator.data_collator)
-        
-        self.val_dataloader = DataLoader(self.val_dataset,
-                                         batch_size=self.cfg.training.batch_size_val,
-                                         sampler=val_sampler,
-                                         collate_fn=DataCollator.data_collator)
-        
-        self.test_dataloader = DataLoader(self.test_dataset,
-                                          batch_size=self.cfg.training.batch_size_val,
-                                          sampler=test_sampler,
-                                          collate_fn=DataCollator.data_collator)
-        
-    def setup_optimizers(self):
-        
-        self.optimizer = AdamW(
-            self.model.parameters(),
-            lr=self.cfg.training.learning_rate,
-            betas=self.cfg.training.betas,
-            eps=self.cfg.training.eps
-        )
-
-        effective_batch_size = self.cfg.training.batch_size_train
-        max_steps = (len(self.train_dataset) // effective_batch_size) * self.cfg.training.num_epochs
-
-        self.scheduler = get_linear_schedule_with_warmup(
-            self.optimizer,
-            num_warmup_steps=self.cfg.training.warmup_steps,
-            num_training_steps=max_steps
-        )
 
     def __init__(
             self,
             cfg: Config
     ):
         super().__init__()
-        #! Current setup assumes a very specific order of initialization.
+        self.tokenizer = None
+        self.feature_extractor = None
+        self.device = None
+        self.train_dataset = None
+        self.val_dataset = None
+        self.test_dataset = None
+        self.train_dataloader = None
+        self.val_dataloader = None
+        self.test_dataloader = None
+        self.optimizer = None
+        self.scheduler = None
+        self.model = None
+
+        # ! Current setup assumes a very specific order of initialization.
         self.cfg = cfg
-        
+
         torch.set_float32_matmul_precision(cfg.torch.float32_matmul_precision)
 
         # Assigns variables for model, tokenizer, feature_extractor
@@ -178,6 +83,109 @@ class VisionEncoderDecoderTrainer(AbstractTrainer):
         # Store average validation loss and BLEU score for potential future reference
         self.avg_val_loss = None
         self.avg_bleu = None
+
+    def setup_model(self):
+        """
+        Set up the VisionEncoderDecoderModel and tokenizer
+        """
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self.cfg.model.tokenizer_name)
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+        self.feature_extractor = AutoFeatureExtractor.from_pretrained(self.cfg.model.feature_extractor)
+
+        # Set up te vision encoder decoder model.
+        self.model = VisionEncoderDecoderModel.from_encoder_decoder_pretrained(
+            self.cfg.model.encoder_name,
+            self.cfg.model.decoder_name
+        )
+
+        self.model.config.decoder_start_token_id = self.tokenizer.bos_token_id
+        self.model.config.pad_token_id = self.tokenizer.pad_token_id
+        self.model.config.eos_token_id = self.tokenizer.eos_token_id
+
+        self.model.config.no_repeat_ngram_size = self.cfg.decoding.no_repeat_ngram_size
+        self.model.config.length_penalty = self.cfg.decoding.length_penalty
+
+        self.model.config.max_length = self.cfg.decoding.max_length
+        self.model.config.early_stopping = self.cfg.decoding.early_stopping
+        self.model.config.num_beams = self.cfg.decoding.num_beams
+
+        self.model.decoder.resize_token_embeddings(len(self.tokenizer))
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        logger.info(f"Using device: {self.device}")
+
+        self.model.to(self.device)
+        torch.compile(self.model)
+
+    def setup_dataset(self):
+
+        dataset = load_dataset(self.cfg.dataset.train_dataset_path, self.cfg.dataset.split_dataset_name)
+        train_val_split = dataset["train"].train_test_split(test_size=self.cfg.dataset.val_test_size,
+                                                            seed=self.cfg.torch.seed)
+        train_ds = train_val_split["train"]
+        val_test_split = train_val_split["test"].train_test_split(test_size=0.5, seed=self.cfg.torch.seed)
+        val_ds = val_test_split["train"]
+        test_ds = val_test_split["test"]
+
+        image_size = self.cfg.image.image_size
+
+        self.train_dataset = LatexDataset(train_ds,
+                                          self.tokenizer,
+                                          self.feature_extractor,
+                                          phase='train',
+                                          image_size=image_size)
+
+        self.val_dataset = LatexDataset(val_ds, self.tokenizer,
+                                        self.feature_extractor,
+                                        phase='val',
+                                        image_size=image_size)
+
+        self.test_dataset = LatexDataset(test_ds,
+                                         self.tokenizer,
+                                         self.feature_extractor,
+                                         phase='test',
+                                         image_size=image_size)
+
+        train_sampler = RandomSampler(self.train_dataset)
+        val_sampler = SequentialSampler(self.val_dataset)
+        test_sampler = SequentialSampler(self.test_dataset)
+
+        DataCollator.padding_value = self.tokenizer.pad_token_id
+
+        self.train_dataloader = DataLoader(self.train_dataset,
+                                           batch_size=self.cfg.training.batch_size_train,
+                                           sampler=train_sampler,
+                                           collate_fn=DataCollator.data_collator)
+
+        self.val_dataloader = DataLoader(self.val_dataset,
+                                         batch_size=self.cfg.training.batch_size_val,
+                                         sampler=val_sampler,
+                                         collate_fn=DataCollator.data_collator)
+
+        self.test_dataloader = DataLoader(self.test_dataset,
+                                          batch_size=self.cfg.training.batch_size_val,
+                                          sampler=test_sampler,
+                                          collate_fn=DataCollator.data_collator)
+
+    def setup_optimizers(self):
+
+        self.optimizer = AdamW(
+            self.model.parameters(),
+            lr=self.cfg.training.learning_rate,
+            betas=self.cfg.training.betas,
+            eps=self.cfg.training.eps
+        )
+
+        effective_batch_size = self.cfg.training.batch_size_train
+        max_steps = (len(self.train_dataset) // effective_batch_size) * self.cfg.training.num_epochs
+
+        self.scheduler = get_linear_schedule_with_warmup(
+            self.optimizer,
+            num_warmup_steps=self.cfg.training.warmup_steps,
+            num_training_steps=max_steps
+        )
 
     def train(self) -> 'AbstractTrainer':
         """
@@ -228,7 +236,7 @@ class VisionEncoderDecoderTrainer(AbstractTrainer):
         interval_losses = []
 
         for step, batch in enumerate(
-            tqdm(self.train_dataloader, desc=f"Epoch {epoch + 1}/{self.num_epochs}")
+                tqdm(self.train_dataloader, desc=f"Epoch {epoch + 1}/{self.num_epochs}")
         ):
             loss = self._train_one_step(batch)
             interval_losses.append(loss)
@@ -238,9 +246,9 @@ class VisionEncoderDecoderTrainer(AbstractTrainer):
 
             # Periodic evaluation and logging
             if global_step % self.eval_steps == 0 or (
-                epoch == self.num_epochs - 1 and step == total_steps_per_epoch - 1):
+                    epoch == self.num_epochs - 1 and step == total_steps_per_epoch - 1):
                 self._log_and_evaluate(global_step, interval_losses, train_losses)
-                
+
         self.save_model(os.path.join(self.checkpoint_dir, f"checkpoint_step_{global_step}"))
 
         return interval_losses
@@ -301,7 +309,6 @@ class VisionEncoderDecoderTrainer(AbstractTrainer):
             self.best_val_loss = val_loss
             self.save_model(os.path.join(self.checkpoint_dir, f"best_checkpoint_step_{global_step}"))
 
-
     def evaluate(self, use_full_eval=False) -> tuple:
         """
         Evaluate the VisionEncoderDecoderModel.
@@ -312,7 +319,7 @@ class VisionEncoderDecoderTrainer(AbstractTrainer):
         val_losses = []
         bleu_scores = []
         num_evaluated_batches = 0
-        
+
         # Display purposes
         total_batches = len(self.val_dataloader) if use_full_eval else self.eval_max_batches
 
@@ -327,17 +334,17 @@ class VisionEncoderDecoderTrainer(AbstractTrainer):
                 pixel_values = batch["pixel_values"].to(self.device)
                 labels = batch["labels"].to(self.device)
 
-                outputs = self.model(pixel_values=pixel_values, labels=labels)              
+                outputs = self.model(pixel_values=pixel_values, labels=labels)
                 val_losses.append(outputs.loss.item())
 
                 # Generate predictions
                 generated_ids = self.model.generate(pixel_values)
-                
+
                 generated_texts = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
                 label_texts = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
 
                 bleu = self.bleu_metric.compute(predictions=generated_texts, references=label_texts)
-                
+
                 bleu_scores.append(bleu["google_bleu"])
 
                 num_evaluated_batches += 1
